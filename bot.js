a932b974600e0396565cc42b8ba98d5791fd6f66
furchill <furchill24@gmail.com>
2025-04-02 11:32:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,9 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const startCommand = require('./commands/start');
 const priceCommand = require('./commands/price');
 const swapCommand = require('./commands/swap');
+const connectCommand = require('./commands/connect');
+
+bot.command('connect', connectCommand);
 
 bot.start(startCommand);
 bot.command('price', priceCommand);
