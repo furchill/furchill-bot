54a0f7b71358f75cad2180ebf478e9244b00d935
furchill <furchill24@gmail.com>
2025-04-02 12:11:06 +0100

    Update walletConnect.js

--- utils/walletConnect.js
+++ utils/walletConnect.js
@@ -1,16 +1,16 @@
 const SignClient = require('@walletconnect/sign-client').default;
 
-let client;
+let client = null;
 
-async function initClient() {
+async function getClient() {
   if (!client) {
     client = await SignClient.init({
       projectId: 'b2beb3c13cc21dfd6aca3182b5812d65',
       metadata: {
-        name: "FurchillBot",
-        description: "Telegram bot for $FURCHILL wallet connection",
-        url: "https://furchill.com",
-        icons: ["https://furchill.com/icon.png"]
+        name: 'FurchillBot',
+        description: 'Telegram bot for $FURCHILL wallet connection',
+        url: 'https://furchill.com',
+        icons: ['https://furchill.com/icon.png']
       }
     });
   }
@@ -18,14 +18,14 @@ async function initClient() {
 }
 
 async function createSession() {
-  const client = await initClient();
+  const client = await getClient();
 
   const { uri, approval } = await client.connect({
     requiredNamespaces: {
       eip155: {
-        methods: ["eth_sendTransaction", "personal_sign"],
-        chains: ["eip155:1"],
-        events: ["accountsChanged", "chainChanged"]
+        methods: ['eth_sendTransaction', 'personal_sign'],
+        chains: ['eip155:1'],
+        events: ['accountsChanged', 'chainChanged']
       }
     }
   });
