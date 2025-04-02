d05f38d144ad6c372e5ba269cde8356b9fd7077c
furchill <furchill24@gmail.com>
2025-04-02 11:22:40 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const startCommand = require('./commands/start');
+const priceCommand = require('./commands/price');
+const swapCommand = require('./commands/swap');
+
+bot.start(startCommand);
+bot.command('price', priceCommand);
+bot.command('swap', swapCommand);
+
+bot.launch();
+console.log('FurchillBot is live!');
